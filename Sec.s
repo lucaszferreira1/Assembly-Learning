c60b5fd7bfd0a99e374362795589eb1198fbe21f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-08-09 10:16:54 -0300

    Update Sec.s

--- Sec.s
+++ Sec.s
@@ -11,30 +11,30 @@ main:
 	li $s2 0 # Cont
 	
 while:
-	li $v0 5
-	syscall 
-	move $s0, $v0 # lê int
+	li $v0 5 # lê int
+	syscall
+	move $s0, $v0
 	
 	add $s1, $s0, $s1 # add int na soma
 	add $s2, $s2, 1 # add 1 na cont
 
 	bne $s0, -1, while
 end:
-	addi $s1, $s1, 1
-	addi $s2, $s2, -1
+	addi $s1, $s1, 1 # add 1 na soma 
+	addi $s2, $s2, -1 # remove 1 da cont
 	
 	li $v0, 1
 	move $a0, $s1
     	syscall
     	
-    	li $v0, 4
+    	li $v0, 4 # imprime nova linha
 	la $a0, newline
 	syscall
 	
-	beqz $s1, erro
+	beqz $s1, erro # se soma = 0 print erro
 	
-     	div $s2, $s1, $s2
-    	li $v0, 1
+     	div $s2, $s1, $s2 # senão print calc media
+    	li $v0, 1 #imprime media
 	move $a0, $s2
     	syscall
     	
