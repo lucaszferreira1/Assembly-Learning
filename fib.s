08cd60e4d2ca537b61278cd0f9a56119afe8abf8
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-08-28 11:44:45 -0300

    Update fib.s

--- fib.s
+++ fib.s
@@ -1,39 +1,50 @@
 	.text
 	.globl main
 main:
-	li $v0 5 # lê int
+	li $v0, 5 # lê int
 	syscall
 	move $a0, $v0
 	
 	bnez $a0, chamada
-	li $s0, 1
+	li $v0, 1
 	j end
 chamada:
-	li $s0, 1
-	jal fibonacci
+	jal fib
 end:
+	move $s0, $v0
 	li $v0, 1 #imprime digitos
 	move $a0, $s0
     	syscall
     	
 	li $v0, 10
 	syscall
-	
+
 fibonacci:
-	beqz $a0, fib_parada
+	bgt $a0, 1, conti
+	jr $ra
+conti:	
 	addi $sp, $sp, -4 # colocamos o endereço na pilha
 	sw $ra, 0($sp)
 	
 	addi $a0, $a0, -1 # desincrementa 1 de a0
-	add $s0, $s0, $a1 # adiciona anterior à soma atual
-	la $a2, ($s0) # carrega s0 em a2
-	sub $a2, $s0, $a1 # subtrai a1 de s0 e carrega em a2
-	la $a1, ($a2) # carrega a2 em a1
+	add $v0, $v0, $a1 # adiciona anterior à soma atual
+	move $t0, $v0 # carrega v0 em t0
+	sub $t0, $v0, $a1 # subtrai a1 de v0 e carrega em t0
+	move $a1, $t0 # carrega t0 em a1
 	jal fibonacci
 	
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4 # retiramos o endereço da pilha
 	jr $ra
+
+fib:
+	addi $sp, $sp, -4 # colocamos o endereço na pilha
+	sw $ra, 0($sp)
 	
-fib_parada:
+	
+	ori $v0, $zero, 1
+	jal fibonacci
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4 # retiramos o endereço da pilha
 	jr $ra
